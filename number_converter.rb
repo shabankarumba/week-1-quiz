0996e42e7235c238345d20249da70ce5260e9c2e
Shaban Karumba <shanban.karumba@fundingcircle.com>
2014-03-06 18:00:17 +0000

    completed test

--- number_converter.rb
+++ number_converter.rb
@@ -1,3 +1,10 @@
 def convert_number(number)
-  # ???
+  if number > 5 
+    puts 3 * number
+  elsif number <= 5 
+    puts number - 3
+  end
 end
+
+convert_number(1)
+convert_number(6)
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,7 +1,23 @@
 require './robot'
 
 describe Robot do
+  subject {
+    Robot.new
+  }
 
+  describe "#greet" do
+    it "greet returns the string \"HELLO ADA\" " do
+      subject.greet('Ada').should == "HELLO ADA"
+    end
+  end
 
+  describe "#calculate" do
+    it "calculate returns 9" do
+      subject.calculate(4,5).should == 9
+    end
 
+    it "calculate returns 5" do
+      subject.calculate(5, 'horse').should == 5
+    end
+  end
 end
